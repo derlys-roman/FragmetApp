8a2a523963a11ab29f72f1b8ef1e7acaa7ae1f3c
Derlys <derlysr6@gmail.com>
2022-04-16 06:08:40 -0300

    Add method OnClickFragmentButton(), and implement in the MainActivity

--- app/src/main/java/com/example/fragmentapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/fragmentapp/activities/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.fragmentapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fragmentapp.R
+import com.example.fragmentapp.interfaces.OnFragmentActionListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnFragmentActionListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClickFragmentButton() {
+        TODO("Not yet implemented")
     }
 }
 //https://cursokotlin.com/capitulo-22-fragments-en-kotlin/
--- app/src/main/java/com/example/fragmentapp/interfaces/OnFragmentActionListener.kt
+++ app/src/main/java/com/example/fragmentapp/interfaces/OnFragmentActionListener.kt
@@ -1,4 +1,5 @@
 package com.example.fragmentapp.interfaces
 
 interface OnFragmentActionListener {
+    fun onClickFragmentButton()
 }
