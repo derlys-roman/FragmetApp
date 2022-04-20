a2ebbb1d25075819744e8c281f8f4f6b32775ad4
Derlys <derlysr6@gmail.com>
2022-04-20 06:51:52 -0300

    add viewBinding on MainActivity, add new color and add new method for show the fragment into the screen

--- app/src/main/java/com/example/fragmentapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/fragmentapp/activities/MainActivity.kt
@@ -3,18 +3,35 @@ package com.example.fragmentapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.fragmentapp.R
+import com.example.fragmentapp.databinding.ActivityMainBinding
+import com.example.fragmentapp.fragments.BlankFragment
+import com.example.fragmentapp.fragments.BlueFragment
 import com.example.fragmentapp.interfaces.OnFragmentActionListener
 
 class MainActivity : AppCompatActivity(), OnFragmentActionListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnBlue.setOnClickListener { loadFragment(BlueFragment()) }
+        binding.btnRed.setOnClickListener { loadFragment(BlankFragment()) }
     }
 
     override fun onClickFragmentButton() {
-        Toast.makeText(this, "The button has clicked", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,
+            "The button has clicked",
+            Toast.LENGTH_SHORT).show()
 
     }
+
+    private fun loadFragment(fragment: Fragment){
+        val fragmentTransaction =
+            supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
 }
-//https://cursokotlin.com/capitulo-22-fragments-en-kotlin/
+    //https://cursokotlin.com/capitulo-22-fragments-en-kotlin/
